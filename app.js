959e580d2c0458a16492a6bbaaeec657772fdd9c
guepardo <bsinet@hotmail.com>
2016-11-24 20:11:34 -0200

    testando scrapper

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var Nightmare = require('nightmare');
 var nightmare = Nightmare({ show: true });
 var vo        = require('vo'); 
+var fs        = require('fs'); 
 
-const URL = 'http://www.zapimoveis.com.br/venda/casas/go+goiania/'
-
+// const URL = 'http://www.zapimoveis.com.br/venda/casas/go+goiania/'
+const URL = 'http://www.zapimoveis.com.br/venda/imoveis/go+goiania/#{"precomaximo":"2147483647","parametrosautosuggest":[{"Bairro":"","Zona":"","Cidade":"GOIANIA","Agrupamento":"","Estado":"GO"}],"pagina":"1","ordem":"Relevancia","paginaOrigem":"ResultadoBusca","semente":"1632872624","formato":"Lista"}'; 
 var list = []; 
 
 //Zap structure: 
@@ -27,44 +28,51 @@ var list = [];
                                                           - > li[0] (rooms), li[1] (suites), li[2] (parking spaces), li[3] (m²)
 */
 
+//function to interact with the body of Zap
 function evaluate(){
     var temp = $('#list > div.box-default > figure.img-container > div.gallery-item-overlay.no-show > div.list-cell'); 
-    
     var array = []; 
 
-    var house ={
-      price : '', 
-      sector: '', 
-      street: '', 
-      city  : '',
-      type  : '', 
-      room  : '',
-      suite : '', 
-      park  : '', 
-      m2    : ''
-    }
+    for(var a = 0; a < temp.length; a++){
+      var house ={
+          price : '', 
+          sector: '', 
+          street: '', 
+          city  : '',
+          type  : '', 
+          room  : '',
+          suite : '', 
+          park  : '', 
+          m2    : ''
+      }; 
 
-    temp.each(function(id, val){
-      house.price = $($(this).find('h3 > span')[0]).text(); 
+      house.price = $($(temp[a]).find('h3 > span')[0]).text(); 
 
-      var spans   = $(this).find('h2 > span'); 
+      var spans   = $(temp[a]).find('h2 > span'); 
 
       house.sector = $(spans[0]).text();  
       house.street = $(spans[1]).text();  
       house.city   = $(spans[2]).text();  
       house.type   = $(spans[3]).text();  
 
-      var lis      = $(this).find('ul.inline > li'); 
+      var lis      = $(temp[a]).find('ul.inline > li'); 
 
       house.room   = $(lis[0]).text();
       house.suite  = $(lis[1]).text();
       house.park   = $(lis[2]).text();
       house.m2     = $(lis[3]).text();  
-      // console.log(house); 
+
       array.push(house); 
-    });
+    }
 
     return array; 
+}
+
+//function to interact with result function above. 
+var list = []; 
+function proccess(result){
+  for(var b of result)
+      fs.appendFile('./data/data_zap.txt', JSON.stringify(b)+'\n', 'utf8', (error) => console.log(error));
 }
 
 function* scrap(){
@@ -79,9 +87,7 @@ function* scrap(){
   
     yield nightmare
           .evaluate(evaluate)
-          .then(function(result){
-            console.log(result); 
-          }); 
+          .then(proccess); 
     
 
     yield nightmare
